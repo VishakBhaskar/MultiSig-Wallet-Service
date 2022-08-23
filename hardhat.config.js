33bab5138b3d6a937897522ff933274734862409
Vishak Bhaskar <vishakbhaskar.619@gmail.com>
2022-08-23 23:29:28 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  solidity: {
+    version: "0.8.13",
+  },
+};
